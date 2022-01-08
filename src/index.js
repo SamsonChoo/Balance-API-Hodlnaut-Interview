17aad384ec97d6985fc69575782c79b8c16f894c
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2022-01-08 18:02:48 +0800

    Refactor getCoinPrice function

--- src/index.js
+++ src/index.js
@@ -6,33 +6,24 @@ import userBalances from "../mock/index.js";
 const app = express();
 const port = 3000;
 
-const getBTCPrice = async () => {
-  try {
-    const res = await axios.get(
-      "https://www.bitstamp.net/api/v2/ticker/btcusd"
-    );
-    const {
-      data: { last: latestBTCPrice },
-    } = res;
-    return latestBTCPrice;
-  } catch (err) {
-    logger.error(err);
-    return -1;
-  }
-};
-
-const getETHPrice = async () => {
-  try {
-    const res = await axios.get(
-      "https://www.bitstamp.net/api/v2/ticker/ethusd"
-    );
-    const {
-      data: { last: latestETHPrice },
-    } = res;
-    return latestETHPrice;
-  } catch (err) {
-    logger.error(err);
-    return -1;
+const getCoinPrice = async (coin) => {
+  if (coin.toLowerCase() === "eth" || coin.toLowerCase() === "btc") {
+    try {
+      const res = await axios.get(
+        `https://www.bitstamp.net/api/v2/ticker/${coin}usd`
+      );
+      const {
+        data: { last: latestBTCPrice },
+      } = res;
+      return Number(latestBTCPrice);
+    } catch (err) {
+      logger.error(err);
+      return -1;
+    }
+  } else {
+    const e = new Error("Invalid coin type");
+    logger.error(e);
+    throw e;
   }
 };
 
@@ -43,8 +34,8 @@ app.get("/", (_req, res) => {
 });
 
 app.get("/balance/:userId", async (req, res) => {
-  const BTCPrice = await getBTCPrice();
-  const ETHPrice = await getETHPrice();
+  const BTCPrice = await getCoinPrice("btc");
+  const ETHPrice = await getCoinPrice("eth");
   const userBTC = Number(userBalances[req.params.userId].BTC);
   const userETH = Number(userBalances[req.params.userId].ETH);
   const userBalanceUSD = userBTC * BTCPrice + userETH * ETHPrice;
