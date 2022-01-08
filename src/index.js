fd33fdfb64c39c34fbd9214e239c9e23813d6015
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2022-01-08 16:21:00 +0800

    Store dummy user balance data

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+// eslint-disable-next-line no-unused-vars
+const userBalances = {
+  "user-1": {
+    BTC: "0.5",
+    ETH: "2",
+  },
+  "user-2": {
+    BTC: "0.1",
+  },
+  "user-3": {
+    ETH: "5",
+  },
+};
+
+app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Example app listening at http://localhost:${port}`);
 });
